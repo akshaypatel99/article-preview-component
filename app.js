ca99fcb802f6ba783ed88e3ec6e9210cb69d78ab
akshaypatel99 <akshaypatel99@gmail.com>
2022-12-16 22:50:14 +0000

    Refactor and improve accessibility

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const shareBtn = document.querySelector('.share');
+const shareBtn = document.querySelector('.share-button');
 let shareSVG = document.getElementById('share-svg');
 let svgPath = document.getElementById('share-svgPath');
-let linksEl = document.querySelector('.share-links');
+let linksContainer = document.querySelector('.share-links');
 
-shareBtn.onclick = () => {
-	linksEl.style.display = linksEl.style.display === 'flex' ? 'none' : 'flex';
-	svgPath.style.fill =
-		svgPath.style.fill === 'rgb(236, 242, 248)'
-			? 'rgb(72, 85, 106)'
-			: 'rgb(236, 242, 248)';
-	shareBtn.style.backgroundColor =
-		shareBtn.style.backgroundColor === 'rgb(108, 127, 151)'
-			? 'rgb(236, 242, 248)'
-			: 'rgb(108, 127, 151)';
-};
+shareBtn.addEventListener('click', () => {
+	const visibility = linksContainer.getAttribute('data-visible');
+
+	if (visibility === 'false') {
+		linksContainer.setAttribute('data-visible', true);
+		shareBtn.setAttribute('aria-expanded', true);
+		svgPath.style.fill = 'rgb(236, 242, 248)';
+		shareBtn.style.backgroundColor = 'rgb(108, 127, 151)';
+	} else {
+		linksContainer.setAttribute('data-visible', false);
+		shareBtn.setAttribute('aria-expanded', false);
+		svgPath.style.fill = 'rgb(72, 85, 106)';
+		shareBtn.style.backgroundColor = 'rgb(236, 242, 248)';
+	}
+});
